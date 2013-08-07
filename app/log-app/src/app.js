a5d4919cdebeae86859fe2532c568dfd037e6469
kangpangpang <kangpangpang@gmail.com>
2013-08-07 17:14:15 +0800

    U: update log

--- app/log-app/src/app.js
+++ app/log-app/src/app.js
@@ -2,15 +2,101 @@ define(function(require, exports, module) {
   'use strict';
 
   var angular = require('angularjs')
+  var moment = require('moment')
   var app = angular.module('totoroLog', [])
-  var ngResource = require('ngResource')
 
 
-  app.controller('LogCtrl', ['$scope', '$http', function($scope, $http) {
-      $http.get('/log').success(function(data) {
-          $scope.logs = data 
+  app.controller('overviewCtrl', ['$scope', 'logService', function($scope, logService) {
+
+      $scope.isWeek = true
+      $scope.isAll = false
+
+      $scope.selectDate = function(type) {
+          if (type === 'week') {
+              $scope.isAll = false
+              $scope.isWeek = true
+          } else {
+              $scope.isAll = true
+              $scope.isWeek = false
+          }
+
+          loadLog(type)
+      }
+
+      $scope.selectDate('week')
+
+      function loadLog(date) {
+          if (date === 'week') {
+              date = (moment().subtract('days', 7).format('YYYYMMDD') + '-' +
+                      moment().format('YYYYMMDD'))
+          }
+
+          $scope.loaded = false
+
+          logService.loadLog(date, function(data) {
+              $scope.logs = data
+              $scope.loaded = true
+          })
+      }
+
+      logService.getLatest(function(datas) {
+          $scope.latest = datas
       })
+
+      logService.getMost(function(datas) {
+          $scope.most = datas
+      })
+
   }])
+
+  app.factory('logService', ['$http', function($http) {
+      return {
+          loadLog: function(date, cb) {
+              var url = '/log/'
+              url +=
+              $http.get(url).success(function(data) {
+                  cb(data)
+              })
+          },
+          getLatest: function(cb) {
+              $http.get('/latest').success(function(data) {
+                  cb(data)
+              })
+
+          },
+          getMost: function(cb) {
+              $http.get('/most').success(function(data) {
+                  console.info('da--->', data)
+                  cb(data)
+              })
+          }
+      }
+  }])
+
+  app.factory('socket', function ($rootScope) {
+      var socket = io.connect();
+      return {
+        on: function (eventName, callback) {
+          socket.on(eventName, function () {
+            var args = arguments;
+            $rootScope.$apply(function () {
+              callback.apply(socket, args);
+            });
+          });
+        },
+        emit: function (eventName, data, callback) {
+          socket.emit(eventName, data, function () {
+            var args = arguments;
+            $rootScope.$apply(function () {
+              if (callback) {
+                callback.apply(socket, args);
+              }
+            });
+          })
+        }
+      };
+  });
+
 
 
   return {
